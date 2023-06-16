0f63efaec3930b45e422d967f495e1ec7decfce5
ZubairARooghwall <ZubairStudyTech@gmail.com>
2023-06-16 11:44:44 +0430

    Copy the previous files

--- Study/settings.py
+++ Study/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'flashcard.apps.FlashcardConfig',
-    'crispy_forms'
 ]
 
 # Use custom user model
--- flashcard/urls.py
+++ flashcard/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
+from django.contrib import admin
 from . import views
+
 
 urlpatterns = [
 	# Everything homepage
 	path('', views.home, name="home"),
 	path('settings', views.settings, name='settings'),
-	path('statistics', views.statistics, name='statistics')
-	,
+	path('statistics', views.statistics, name='statistics'),
 	# this is for new users
 	# path('home', views.newvisitor, name="NewVisitor"),
 	
@@ -17,5 +18,8 @@ urlpatterns = [
 	
 	# user management
 	path('delete-account', views.delete_account, name="delete_account"),
+	
+	# todo
+	path('update_todo/<int:todoid>/', views.update_todo, name="update_todo")
 
 ]
